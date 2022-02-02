77d34c50730a6ff1c0e2ecd14fa4088f3c2bf300
Axel Vanraes <axelvanraes@gmail.com>
2022-02-02 08:23:24 +0100

    Adapted README and added config.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Getting Started with the Calculator Boilerplate 
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
@@ -6,7 +6,10 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
-### `npm start`
+### `yarn install`
+Installs the necessary dependencies in order to run or build this app.
+
+### `yarn start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
@@ -14,12 +17,12 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
 
-### `npm test`
+### `yarn test`
 
 Launches the test runner in the interactive watch mode.\
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
+### `yarn build`
 
 Builds the app for production to the `build` folder.\
 It correctly bundles React in production mode and optimizes the build for the best performance.
@@ -29,7 +32,7 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
+### `yarn eject`
 
 **Note: this is a one-way operation. Once you `eject`, you can’t go back!**
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Field, Form, Formik, useFormikContext } from 'formik';
 import './App.css';
 
-function App() {
+const INITIAL_VALUES = {
+  a: 1,
+  b: 2,
+}
+export function Calculator() {
+  const {values} = useFormikContext<typeof INITIAL_VALUES>()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Form className='sm:text-sm'>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div className="mt-2">
+        <label className='mx-1'>
+          Value of <strong>a</strong>:
+        </label>
+        <Field className="rounded-md border-gray-400" type="number" name="a" />
+      </div>
+      <div className="mt-2">
+        <label className='mx-1'>
+          Value of <strong>b</strong>:
+        </label>
+        <Field className="rounded-md border-gray-400" type="number" name="b" />
+      </div>
+      <div className="mt-2">
+        <label className='mx-1'>
+          Value of <strong>c</strong>:
+        </label>
+        <Field className="rounded-md border-gray-400 disabled:bg-gray-50" type="number" name="c" value={values.a + values.b} disabled />
+      </div>
+    </Form>
+  )
 }
 
-export default App;
+export default function App() {
+  return (
+    <div id="page-container" className="px-5 py-3">
+      <Formik initialValues={INITIAL_VALUES} onSubmit={(values) => console.log(values)}>
+        <Calculator />
+      </Formik>
+    </div>
+  )
+}
